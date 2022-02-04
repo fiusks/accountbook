8ba49c8bcfa309b6508df80e7eff15bd1b4fadc4
Thiago Ferreira <tllucasferreira15@gmail.com>
2022-02-04 00:41:29 -0300

    teste de deploy

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express();
 
 routes.get('/', (req,res) => {
-    res.send('(hml) deu tudo certo!');
+    res.send('(hml) deu tudo certo! ');
 })
 
 module.exports = routes;
