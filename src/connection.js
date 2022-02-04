294d05d8925572bae186cea816d3501fb6dc586d
Thiago Ferreira <tllucasferreira15@gmail.com>
2022-02-04 03:35:13 -0300

    configurando conexão com DB sem variaveis de ambiente

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: 'xlaqwvpmmvponh',
+    host: 'ec2-54-157-15-228.compute-1.amazonaws.com',
+    database: 'dddom8q67sbv5j',
+    password: '7b936a02f55596666a2f791e1e6558c0d36aa31173bc77196748ef27ede56045',
+    port: 5432,
     ssl: {
         rejectUnauthorized: false
     }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const app = require('./server');
 
 app.listen(process.env.PORT || 3000);
