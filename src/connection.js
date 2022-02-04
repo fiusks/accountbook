96655b3db1f1dd26dc9ef9565c8dfa7d2b321503
Thiago Ferreira <tllucasferreira15@gmail.com>
2022-02-04 02:33:07 -0300

    testando conexao com banco de dados

--- src/connection.js
+++ src/connection.js
@@ -2,6 +2,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         host: 'ec2-54-157-15-228.compute-1.amazonaws.com',
+        port: 5432,
         user: 'xlaqwvpmmvponh',
         password: '7b936a02f55596666a2f791e1e6558c0d36aa31173bc77196748ef27ede56045',
         database: 'dddom8q67sbv5j'
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const knex = require('./connection');
 const routes = express();
 
 routes.get('/', async (req,res) => {
-    const testeProducao = await knex('usuarios')
+    const testeProducao = await knex('usuarios');
     res.json({ mensagem: '(master) deu tudo certo!', testeProducao});
 })
 
