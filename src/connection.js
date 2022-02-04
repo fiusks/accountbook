9d6282c3184455b1c628e73853b850f27e418637
Thiago Ferreira <tllucasferreira15@gmail.com>
2022-02-04 01:58:54 -0300

    configuração de conexão bando de dados produção

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'knexjs'
+        host: 'ec2-54-157-15-228.compute-1.amazonaws.com',
+        user: 'xlaqwvpmmvponh',
+        password: '7b936a02f55596666a2f791e1e6558c0d36aa31173bc77196748ef27ede56045',
+        database: 'dddom8q67sbv5j'
     }
 })
 
