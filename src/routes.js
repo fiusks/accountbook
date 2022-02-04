0e9d6948b03073a8a16a99de31ec60f0220293fc
Thiago Ferreira <tllucasferreira15@gmail.com>
2022-02-04 02:26:45 -0300

    teste de conexao com banco de dados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const knex = require('./connection');
 
 const routes = express();
 
-routes.get('/', (req,res) => {
-    res.send('(master) deu tudo certo!');
+routes.get('/', async (req,res) => {
+    const testeProducao = await knex('usuarios')
+    res.json({ mensagem: '(master) deu tudo certo!', testeProducao});
 })
 
 module.exports = routes;
