3d1810647ce4c0ae86676f2d2b3dc690b52e36a0
Thiago Ferreira <tllucasferreira15@gmail.com>
2022-02-04 03:15:19 -0300

    configurando variaveis de ambiente

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    user: 'xlaqwvpmmvponh',
-    host: 'ec2-54-157-15-228.compute-1.amazonaws.com',
-    database: 'dddom8q67sbv5j',
-    password: '7b936a02f55596666a2f791e1e6558c0d36aa31173bc77196748ef27ede56045',
-    port: 5432,
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
     ssl: {
         rejectUnauthorized: false
     }
