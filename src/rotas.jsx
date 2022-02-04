e4f1dd4a7c707aabd584ef8512ec8269ebe1d143
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-04 13:11:36 -0300

    correção no path de login

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -8,7 +8,7 @@ function Rotas() {
     return (
         <AuthProvider>
             <Routes>
-                <Route path='/' element={<Login/>} />
+                <Route path='/login' element={<Login/>} />
                 <Route path='/home' element={<Home/>} />
                 <Route path='/logout' element={<LogOut/>} />
             </Routes>
