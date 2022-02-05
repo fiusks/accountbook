3a4e78a0213eb0170937e1dd1ec1a48760fa34f6
christian garrido <christianthir@hotmail.com>
2022-02-04 22:22:17 -0300

    Adicinando EndPoint de Login Feito por Bernardo e refatorado por Chris.

--- src/Controllers/Controller.js
+++ src/Controllers/Controller.js
@@ -1,6 +1,8 @@
 const knex = require('../connection');
 const yup = require('yup')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const secret = require('./config')
 const verifyEmail = async (req,res) => {
     const email = req.query.email
     if (!email) {
@@ -15,7 +17,7 @@ const verifyEmail = async (req,res) => {
         res.status(400).json({message: 'E-mail already exists in database.'});
     
 }
-const registerUser = async(req, res) => {
+const registerUser = async (req, res) => {
     const schema = yup.object().shape({
         nome: yup.string().required(),
         email: yup.string().email().required(),
@@ -39,9 +41,49 @@ const registerUser = async(req, res) => {
         return res.status(400).json(error.message)
     }
 }
+const login = async (req, res) => {
+    const {email, senha} = req.body;
+    const schema = yup.object().shape({
+        email: yup.string().email().required("E-mail is required to Login"),
+        senha: yup.string().required("Password is required to Login")
+    });
+    try {
+        await schema.validate(req.body);
+        const findUserEmail = await knex('usuarios').where('email', email).debug();
+
+        if (findUserEmail.length === 0) {
+            return res.status(404).json({message: `User with E-mail ${email}, not found.`});
+        }
+
+        const foundUser = findUserEmail[0];
+        const checkPassword = await bcrypt.compare(senha, foundUser.senha);
+        console.log(checkPassword)
+        if (!checkPassword) {
+            return res.status(401).json({message: `Password doesn't check with E-mail ${email}.`});
+        }
+        
+        const {senha:_, ...userData} = foundUser;
+        
+        const token = jwt.sign(
+            userData, 
+            secret,
+            {expiresIn: '1h'}
+        ); 
+        
+
+        return res.status(200).json({
+            message: 'Login efetuado com sucesso',
+            token: token
+        });
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
 
 module.exports = {
     verifyEmail,
     registerUser,
+    login
 
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { verifyEmail, registerUser } = require('./Controllers/Controller.js')
+const { verifyEmail, registerUser, login } = require('./Controllers/Controller.js')
 const routes = express();
 
 routes.get('/', verifyEmail); // rota de verificação se o email informado ja existe no banco de dados 
 routes.post('/signUp', registerUser) // rota de cadastro de novo usuário.
-// routes.post('/login', login)
+routes.post('/login', login) // rota de geração de token para autorização 
+
 module.exports = routes;
