0a330eb38ade5cfb789519663d071812ea2ba2dd
fiusks <rafabarros1@hotmail.com>
2022-02-05 22:57:46 -0300

    Atualização da página home com os componentes de Card junto com o layout (navBar e Header)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,30 @@
+import "./style.scss";
+import CardOverview from "../../components/card-overview";
+import CardDeDados from "../../components/card-cobrancas";
+
 function Home() {
-    return (<div className="home">
-    <h1>home</h1>
-    </div>);
+  return (
+    <div className="home-container">
+      <div className="cards-overview-container">
+        <CardOverview key="resumo pagas" cardType="pagas" value="30000" />
+        <CardOverview key="resumo vencidas" cardType="vencidas" value="30000" />
+        <CardOverview
+          key="resumo previstas"
+          cardType="previstas"
+          value="30000"
+        />
+      </div>
+      <div className="cards-cobranca-container">
+        <CardDeDados cardType="pagas" />
+        <CardDeDados cardType="vencidas" />
+        <CardDeDados cardType="previstas" />
+      </div>
+      <div className="cards-clientes-container">
+        <CardDeDados cardType="inadimplente" />
+        <CardDeDados cardType="em-dia" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
