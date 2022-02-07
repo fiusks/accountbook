4045ddf922114899b8d80a2d2b48b136cec9e6ba
christian garrido <christianthir@hotmail.com>
2022-02-07 18:38:19 -0300

    Schema atualizado

--- schema.sql
+++ schema.sql
@@ -6,17 +6,21 @@ create table if not exists usuarios (
   cpf text unique,
   telefone text unique
 );
-create tabel if not exists enderecos (
-  id serial primary key,
-
-);
 
 create table if not exists clientes (
   id serial primary key,
   nome text not null,
   email text not null unique,
   cpf text unique,
-  telefone text unique
-  endereco_id int
-  foreign key (endereco_id) references enderecos(id)
+  telefone text,
+  endereco text,
+  estado default 0,
+);
+
+create table if not exists cobrancas (
+  id serial primary key,
+  cliente_id int,
+  valor int,
+  estado default 0,
+  foreign key (client_id) references clientes(id)
 );
