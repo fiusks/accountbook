38f715f863cbd07c6a9a4889ba418e0c84a53659
Thiago Ferreira <tllucasferreira15@gmail.com>
2022-02-08 14:57:02 -0300

    configurando variaveis de ambiente

--- src/connection.js
+++ src/connection.js
@@ -2,11 +2,11 @@ const knex = require('knex')({
     client: 'pg',
     version: '^8.7.1',
     connection: {
-        host: 'ec2-54-157-15-228.compute-1.amazonaws.com',
+        host: process.env.DB_HOST,
         port: 5432,
-        user: 'xlaqwvpmmvponh',
-        password: '7b936a02f55596666a2f791e1e6558c0d36aa31173bc77196748ef27ede56045',
-        database: 'dddom8q67sbv5j',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD ,
+        database: process.env.DB_DATABASE,
         ssl: {
             rejectUnauthorized: false 
         }
