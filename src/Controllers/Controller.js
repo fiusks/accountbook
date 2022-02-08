4a1e1d95c3fcac24c07c538792d0ebee53b11c8f
Baauh <bernardo.miranda@hotmail.com>
2022-02-08 17:59:45 -0300

    Adição do editar usuario

--- src/Controllers/Controller.js
+++ src/Controllers/Controller.js
@@ -92,7 +92,57 @@ const login = async (req, res) => {
 }
 
 const editUser = async(req, res) => {
+    const { nome, email, novaSenha, confirmarSenha, cpf, telefone } = req.body;
+    const { id } = req.usuario;
 
+    if (novaSenha) {
+        if (novaSenha !== confirmarSenha) {
+            return res.status().json("New password and confirm password must be the same.")
+        }
+        const schema = yup.object().shape({
+            nome: yup.string().required("Name is required to edit a user."),
+            email: yup.string().email().required("E-mail is required to edit a user."),
+            novaSenha: yup.string().required("Password is required to edit a user."),
+            confirmarSenha: yup.string().required("Password is required to edit a user.")
+        });
+    } else {
+        const schema = yup.object().shape({
+            nome: yup.string().required("Name is required to edit a user."),
+            email: yup.string().email().required("E-mail is required to edit a user.")
+        });
+    }
+
+    try {
+        await schema.validate(req.body);
+
+        const senha = '';
+
+        novaSenha ? senha = await bcrypt.hash(novaSenha, 10) : '';
+
+        if (email !== req.usuario.email) {
+            const emailUsuarioExiste = await knex('usuarios').where('email', email);
+
+            if (emailUsuarioExiste.length > 0) {
+                return res.status(400).json('The email is already in use.')
+            }
+        }
+
+        const usuarioAtualizado = await knex('usuarios').where('id', id).update({
+            nome: nome,
+            email: email,
+            senha: senha,
+            cpf: cpf,
+            telefone: telefone
+        });
+
+        if (!usuarioAtualizado) {
+            return res.status(400).json('The user has not been updated.');
+        }
+
+        return res.status(200).json('The user has been successfully updated.');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    };
 }
 
 const registerClient = async(req, res) => {
@@ -117,6 +167,7 @@ const registerDebt = async(req, res) => {
 module.exports = {
     verifyEmail,
     registerUser,
-    login
+    login,
+    editUser
 
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { verifyEmail, registerUser, login } = require('./Controllers/Controller.js')
+const { verifyEmail, registerUser, login, editUser } = require('./Controllers/Controller.js')
 const routes = express();
 
 routes.get('/', verifyEmail); // rota de verificação se o email informado ja existe no banco de dados 
 routes.post('/signUp', registerUser) // rota de cadastro de novo usuário.
 routes.post('/login', login) // rota de geração de token para autorização 
+routes.put('/edituser', editUser) // rota de edição de usuarios logados.
 
 module.exports = routes;
