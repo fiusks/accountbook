9a154bf7602e2dc3c76a5003871cc8a0733d1c1e
fiusks <rafabarros1@hotmail.com>
2022-02-08 23:39:50 -0300

    Adição de novos estados para capturar os dados dos clientes

--- src/hooks/useUserProvider.js
+++ src/hooks/useUserProvider.js
@@ -2,14 +2,26 @@ import { useState } from "react";
 
 function useUserProvider() {
   const [openModal, setOpenModal] = useState(false);
+  const [openClientModal, setOpenClientModal] = useState(false);
   const [openEditMenu, setOpenEditMenu] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const [userData, setUserData] = useState({
     name: "",
     email: "",
     cpf: "",
     phone: "",
   });
-
+  const [clientForm, setClientForm] = useState({
+    name: "",
+    email: "",
+    cpf: "",
+    phone: "",
+    address: "",
+    complement: "",
+    zipcode: "",
+    district: "",
+    city: "",
+  });
   return {
     openModal,
     setOpenModal,
@@ -17,6 +29,12 @@ function useUserProvider() {
     setOpenEditMenu,
     userData,
     setUserData,
+    clientForm,
+    setClientForm,
+    formSubmitted,
+    setFormSubmitted,
+    openClientModal,
+    setOpenClientModal,
   };
 }
 
