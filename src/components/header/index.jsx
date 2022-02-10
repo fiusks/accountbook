6dfccc71305bf00072ff7afacd477d93bcd13b11
fiusks <rafabarros1@hotmail.com>
2022-02-10 05:31:12 -0300

    Espaço vazio

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -13,10 +13,9 @@ function Header() {
   const nome = userData.name;
 
   const firstLetters = nome[0].toUpperCase() + nome[2].toUpperCase();
-  
+
   const currentLocation = location.pathname.split("/")[2];
 
-  
   // const firstLetters =
   //   nome.[charAt(0)].toLocaleUpperCase() +
   //   nome.split(" ")[1].charAt(0).toLocaleUpperCase();
@@ -25,8 +24,12 @@ function Header() {
     <header>
       <div className="header-container">
         {currentLocation === "home" && <h1>Resumo das Cobranças</h1>}
-        {currentLocation === "clientes" && <h2 className="greenSmallName">Clientes</h2>}
-        {currentLocation === "cobrancas" && <h2 className="greenSmallName" >Cobranças</h2>}
+        {currentLocation === "clientes" && (
+          <h2 className="greenSmallName">Clientes</h2>
+        )}
+        {currentLocation === "cobrancas" && (
+          <h2 className="greenSmallName">Cobranças</h2>
+        )}
         <div className="user-profile-container">
           <h2 className="image-profile">{firstLetters}</h2>
           <h3>{nome}</h3>
