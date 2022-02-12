7abc149d7a1fab94bdfc69bfc627ae5e4d97b3f2
fiusks <rafabarros1@hotmail.com>
2022-02-12 20:22:00 -0300

    Refatoração do modal de clientes com o yup e formik

---
+++ src/components/client-modal/layout/index.jsx
@@ -0,0 +1,40 @@
+import "./style.scss";
+import { useState } from "react";
+import clientIcon from "../../../assets/images/clientsIcon.svg";
+import { Modal, Button } from "react-bootstrap";
+import ClientForm from "../form";
+
+function ClientModal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        + Adicionar Cliente
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title className="flex-row">
+            <img src={clientIcon} alt="client icon" />
+            <span>Cadastro do Cliente</span>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ClientForm handleClose={handleClose} />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default ClientModal;
