03eb0c318b55b5760649cf51e41d3735573029d4
fiusks <rafabarros1@hotmail.com>
2022-02-14 19:23:54 -0300

    Voltando o cors para o estado original antes das tentativas de modificação

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const routes = require("./router/routes");
 const cors = require("cors");
+const routes = require("./router/routes");
 
 const app = express();
 
