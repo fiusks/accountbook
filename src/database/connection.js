124506dec7a11025e8a73676f97653c70ff097cc
fiusks <rafabarros1@hotmail.com>
2022-02-14 18:30:17 -0300

    Correção da porta do DB

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ const knex = require("knex")({
   version: "^8.7.1",
   connection: {
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
