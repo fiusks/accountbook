e88b2fa5331069b46838db42551a564df07f3521
Baauh <bernardo.miranda@hotmail.com>
2022-02-15 14:30:35 -0300

    Adicionado o detalhamento do cliente e a lista de cobranças do cliente

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,8 @@ const listClients = require("./controllers/clients/listClients");
 const editClient = require("./controllers/clients/editClient");
 const listBills = require("./controllers/bills/listBills");
 const verifyEmail = require("./controllers/users/verifyEmail");
+const getClients = require("./controllers/clients/getClient");
+const listClientBills = require("./controllers/bills/listClientBills");
 
 // rotas de Cadastro/Login VVV
 routes.post("/login", login); // rota de geração de token para autorização
@@ -26,5 +28,9 @@ routes.get("/listClients", checkToken, listClients);
 routes.post("/registerClient", checkToken, registerClient); // rota de cadastro de novo cliente.
 routes.put("/editClient", checkToken, editClient); // rota de edição de cliente cadastrado. FUTURAS IMPLEMENTAÇÕES
 routes.get("/listHome", checkToken, listBills); //listagem dos dados da home
+routes.get("/getClients/:id", checkToken, getClients);
+
+//rotas de Cobranças VVV
+routes.get("/listClientBills/:clientId", checkToken, listClientBills);
 
 module.exports = routes;
