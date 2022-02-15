1a3a04caa1203752b8c5356de188a3ab836ef64d
christian garrido <christianthir@hotmail.com>
2022-02-14 22:12:53 -0300

    Add res.status(400).json(email nao informado) basicamnete

--- src/controllers/users/verifyEmail.js
+++ src/controllers/users/verifyEmail.js
@@ -6,8 +6,11 @@ const verifyEmail =  async (req, res) => {
 
     if (email) {
         const emailCheck = await knex('users').where({email}).first();
-        emailCheck? res.status(200).json({success: "E-mail disponível"}):res.status(400).json({success: "NOT"});
-    } 
+
+        emailCheck? res.status(200).json({success: "E-mail disponível"}) : res.status(400).json({success: "NOT"});
+    } else {
+        res.status(400).json({user: {email: "E-mail não informado"}})
+    }
 }
 
 module.exports = verifyEmail;
