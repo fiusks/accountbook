d52e032254fca11e017dbbec484ffd475543419f
christian garrido <christianthir@hotmail.com>
2022-02-15 23:35:54 -0300

    Criação da função listing bills e schema do BD atualizado

--- schema.sql
+++ schema.sql
@@ -12,21 +12,44 @@ create table if not exists clients (
   name text not null,
   email text not null unique,
   cpf text not null unique,
-  phone not null text,
+  phone text not null,
   address text,
   complement text,
   zipcode text,
   district text,
   city text,
-  state text,
+  state text
 );
-
 
 create table if not exists bills (
   id serial primary key,
   client_ID int not null,
   amount bigint not null,
+  description text not null,
   bill_status text not null,
   due_date date not null,
-  foreign key (cliente_ID) references clients(id)
+  foreign key (client_ID) references clients(id)
 );
+
+drop table bills;
+
+insert into clients (name, email, cpf, phone) values ('Ruli', 'ruli@cubos.com', '12345699606', '71988747675');
+insert into clients (name, email, cpf, phone) values ('Alfredo', 'alfafa@cozinhaArdente.com', '92039475638', '21983658473'); 
+insert into clients (name, email, cpf, phone) values ('Muriel', 'mumu@lugarnenhum.com', '92000475638', '11383658473'); 
+insert into clients (name, email, cpf, phone) values ('Zambi', 'mestre@daginga.com', '33039475638', '71983658473'); 
+
+
+insert into bills (client_id, amount, description, bill_status, due_date) values (2, 200,'aaaaaaaaa', 'pending', '2022-02-17');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (2, 350,'bbbbbbbb', 'pending', '2022-02-18');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (4, 700,'ccccccc', 'pending', '2022-02-23');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (5, 2000,'dddddddd', 'pending', '2022-03-05');
+
+insert into bills (client_Id, amount, description, bill_status, due_date) values (3, 205,'eeeeee', 'pending', '2022-02-11');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (2, 2060,'ffffffff', 'pending', '2022-02-07');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (4, 3510,'ggggggg', 'pending', '2022-01-15');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (3, 150,'hhhhhhh', 'pending', '2021-11-23');
+
+insert into bills (client_Id, amount, description, bill_status, due_date) values (5, 756,'iiiiiii', 'paid', '2022-02-13');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (4, 985,'jjjjjjjjjjj', 'paid', '2022-02-05');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (3, 1004,'kkkkkkkkkk', 'paid', '2022-01-25');
+insert into bills (client_Id, amount, description, bill_status, due_date) values (2, 15,'llllllllllllll', 'paid', '2021-12-21');
--- src/controllers/bills/listBills.js
+++ src/controllers/bills/listBills.js
@@ -1,20 +1,62 @@
 const knex = require("../../database/connection");
 
-const listBills = async (req, res) => {};
+const listBills = async (req, res) => {
 
-// try {
-//   const getPaiddBills = await knex("bills");
-//   const getUnpaidBills = await knex("bills");
-//   const getOverdueBills = await knex("bill");
-//   const getOverdueClients = await knex("bills");
-//   const getInDayClients = await knex("bills");
+    try {
+        const getPaidBills = await knex('bills')
+        .leftJoin('clients', 'clients.id', 'bills.client_id')
+        .select('clients.name', 'bills.id', 'bills.amount')
+        .where({bill_status: 'paid'});
+        const quantityPaidBills = getPaidBills.length;
 
-//   res.status(200).json({
-//     cobrancas: getCobrancasFromDataBase,
-//     clientes: getClientesFromDataBase,
-//   });
-// } catch (error) {
-//   res.status(400).json(error.message);
-// }
+        const getUnpaidBills = await knex('bills')
+        .leftJoin('clients', 'clients.id', 'bills.client_id')
+        .select('clients.name', 'bills.id', 'bills.amount')
+        .where({bill_status: 'pending'})
+        .andWhere('due_date', '>', new Date());
+        const quantityUnpaidBills = getUnpaidBills.length;
+
+        const getOverdueBills = await knex('bills')
+        .leftJoin('clients', 'clients.id', 'bills.client_id')
+        .select('clients.name','bills.client_id', 'bills.id', 'bills.amount', 'bills.due_date')
+        .where({bill_status: 'pending'})
+        .andWhere('due_date', '<', new Date());
+        const quantityOverdueBills = getOverdueBills.length;
+        // /\/\/\ bills
+
+        const getOverdueClients = await knex('bills')
+        .leftJoin('clients', 'clients.id', 'bills.client_id')
+        .select('bills.amount', 'bills.id', 'clients.name', 'bills.due_date')
+        .where({bill_status: 'pending'})
+        .andWhere('due_date', '<', new Date())
+        .groupBy('bills.amount', 'bills.id', 'clients.name');
+        const quantityOverdueClients = getOverdueClients.length;
+
+        const getOndueClients = await knex('bills')
+        .innerJoin('clients', 'clients.id', 'bills.client_id')
+        .select('bills.amount', 'bills.id', 'clients.name', 'bills.due_date')
+        .where({bill_status: 'pending'})
+        .andWhere('due_date', '>', new Date())
+        .groupBy('bills.amount', 'bills.id', 'clients.name').groupBy('clients.name').debug();
+        const quantityOndueClients = getOndueClients.length;
+        console.log(getOndueClients)
+        res.status(200).json({ client: {
+            overdueClients:getOverdueClients,
+            quantityPaidBills:quantityPaidBills < 10? `0${String(quantityPaidBills)}`: String(quantityPaidBills),
+            ondueClients:getOndueClients,
+            quantityUnpaidBills:quantityUnpaidBills < 10?`0${String(quantityUnpaidBills)}`: String(quantityUnpaidBills),
+            overdueBills: getOverdueBills,
+            quantityOverdueBills:quantityOverdueBills < 10?`0${String(quantityOverdueBills)}`: String(quantityOverdueBills),
+            paidBills: getPaidBills,
+            quantityOverdueClients:quantityOverdueClients < 10?`0${String(quantityOverdueClients)}`: String(quantityOverdueClients),
+            unpaidBills: getUnpaidBills,
+            quantityOndueClients:quantityOndueClients < 10?`0${String(quantityOndueClients)}`: String(quantityOndueClients)
+        }});
+     
+      
+    } catch (error) {
+      res.status(400).json(error.message);
+    }
+};
 
 module.exports = listBills;
