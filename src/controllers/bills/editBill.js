5e881666c803d0048e9005dd5f2e166d9a91297f
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-23 12:28:38 -0300

    adicionado a função de editar cobrança

--- src/controllers/bills/editBill.js
+++ src/controllers/bills/editBill.js
@@ -1,5 +1,28 @@
 const knex = require("../../database/connection");
+const billsSchema = require("../../validation/billsSchema");
 
-const editBill = async (req, res) => {};
+const editBill = async (req, res) => {
+  try {
+    await billsSchema.validate(req.body.bill);
+    const { id, clientId, amount, status, dueDate, desc } = req.body.bill;
+    const response = await knex("bills")
+      .where({ id })
+      .update({
+        amount: amount,
+        bill_status: status,
+        client_id: clientId,
+        description: desc,
+        due_date: dueDate,
+      })
+      .returning("*");
+    if (response === 0) {
+      return res
+        .status(400)
+        .json({ message: "it was not possible to register a edited billing" });
+    }
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 module.exports = editBill;
