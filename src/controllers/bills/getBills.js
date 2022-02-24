b4a76439584ede84496fca9059e99607de414476
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-23 22:32:45 -0300

    adicionando client_id no select

--- src/controllers/bills/getBills.js
+++ src/controllers/bills/getBills.js
@@ -1,28 +1,35 @@
-const knex = require('../../database/connection');
+const knex = require("../../database/connection");
 
 const getBills = async (req, res) => {
+  try {
+    const bills = await knex("bills")
+      .leftJoin("clients", "clients.id", "bills.client_id")
+      .select(
+        "clients.name",
+        "clients.id",
+        "bills.id",
+        "bills.amount",
+        "bills.description",
+        "bills.bill_status",
+        "bills.due_date"
+      )
+      .orderBy("bills.id", "desc")
+      .limit(10);
 
-    try {
-        const bills = await knex('bills')
-            .leftJoin('clients', 'clients.id', 'bills.client_id')
-            .select('clients.name', 'bills.id', 'bills.amount', 'bills.description', 'bills.bill_status', 'bills.due_date')
-            .orderBy('bills.id', 'desc').limit(10);
-
-        for (const bill of bills) {
-            if (bill.due_date < new Date() && bill.bill_status !== 'paid') {
-                bill.bill_status = 'overdue'
-            }
-        }
-
-        return res.status(200).json({
-            bills
-        });
-
-    } catch (error) {
-        return res.status(400).json({
-            error: error.message
-        })
+    for (const bill of bills) {
+      if (bill.due_date < new Date() && bill.bill_status !== "paid") {
+        bill.bill_status = "overdue";
+      }
     }
-}
+
+    return res.status(200).json({
+      bills,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message,
+    });
+  }
+};
 
 module.exports = getBills;
