0b1da38afa68135251d63ebd577a79c99c82f414
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-23 21:02:37 -0300

    refatoração de busca de cobrança

--- src/controllers/bills/searchBill.js
+++ src/controllers/bills/searchBill.js
@@ -1,51 +1,35 @@
 const knex = require("../../database/connection");
-const { searchBillSchema } = require("../../validation/billsSchema");
 
 const searchBill = async (req, res) => {
-  await searchBillSchema.validate(req.body.bill);
   try {
-    const { params, category } = req.body.bill;
+    const { params } = req.body.filterBill;
 
-    if (category === "searchById") {
-      const bills = await knex("bills")
-        .leftJoin("clients", "clients.id", "bills.client_id")
-        .select(
-          "clients.name",
-          "bills.id",
-          "bills.amount",
-          "bills.description",
-          "bills.bill_status",
-          "bills.due_date"
-        )
-        .orderBy("bills.id", "desc")
-        .limit(10)
-        .where({ id: params });
-      return res.status(404).json(bills);
+    const bills = await knex("bills")
+      .leftJoin("clients", "clients.id", "bills.client_id")
+      .select(
+        "clients.name",
+        "bills.id",
+        "bills.amount",
+        "bills.description",
+        "bills.bill_status",
+        "bills.due_date"
+      )
+      .orderBy("bills.id", "desc")
+      .limit(10)
+      .where("bills.id", isNaN(params) ? null : params)
+      .orWhere("clients.name", params);
 
-      for (const bill of bills) {
-        if (bill.due_date < new Date() && bill.bill_status !== "paid") {
-          bill.bill_status = "overdue";
-        }
+    for (const bill of bills) {
+      if (bill.due_date < new Date() && bill.bill_status !== "paid") {
+        bill.bill_status = "overdue";
       }
-
-      if (bills.length === 0) {
-        return res.status(404).json({ message: "Bill not found" });
-      }
-      return res.status(200).json(bills);
-    } else {
-      const clientID = await knex("clients").where({ name: params });
-
-      if (clientID.length === 0) {
-        return res.status(404).json({ message: "Client not found" });
-      }
-      const response = await knex("bills").where({ client_id: clientID[0].id });
-      if (response.length === 0) {
-        return res.status(404).json({ message: "Bill not found" });
-      }
-      return res.status(200).json(response);
     }
+    if (bills.length === 0) {
+      return res.status(404).json({ message: "Bill not found" });
+    }
+    return res.status(200).json(bills);
   } catch (error) {
-    return res.status(400).json(error);
+    return res.status(400).json(error.message);
   }
 };
 
