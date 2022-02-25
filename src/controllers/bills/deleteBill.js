e47a86f10955dabdd2276f2204d890407a54cba5
Baauh <bernardo.miranda@hotmail.com>
2022-02-24 21:56:06 -0300

    Delete bill pronto

--- src/controllers/bills/deleteBill.js
+++ src/controllers/bills/deleteBill.js
@@ -4,7 +4,7 @@ const deleteBill = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const cobranca = await knex("bills").where({ id }).first().del();
+        await knex("bills").where({ id }).first().del();
 
         return res.status(200).json("Cobrança deletada com sucesso.");
     } catch (error) {
