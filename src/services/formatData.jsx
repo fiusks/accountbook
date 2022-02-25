c725cd26646bd0f777fd32f4fa740967de29691d
fiusks <rafabarros1@hotmail.com>
2022-02-25 16:26:51 -0300

    Colocação da função de data em serviços

--- src/services/formatData.jsx
+++ src/services/formatData.jsx
@@ -35,6 +35,10 @@ export function formatPhone(phone) {
   }
 }
 
+export function formatDate(date) {
+  return new Intl.DateTimeFormat("pt-BR").format(Date.parse(date) + 10800000);
+}
+
 export function formatToCurrency(inputNumber) {
   const convertedValue = new Intl.NumberFormat("pt-BR", {
     style: "currency",
