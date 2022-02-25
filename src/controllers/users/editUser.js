c20be2fe89be1b2256c0c2475674e916307cf922
fiusks <rafabarros1@hotmail.com>
2022-02-25 17:17:56 -0300

    Envio da atualização do usuário para o front

--- src/controllers/users/editUser.js
+++ src/controllers/users/editUser.js
@@ -5,69 +5,72 @@ const editUser = async (req, res) => {
   const { id, name, email, password, cpf, phone } = req.body.user;
 
   const newUserData = {};
+  const errors = {};
 
   try {
+    const userDB = await knex("users").where({ id }).first();
     const {
       name: nameDB,
       email: emailDB,
       cpf: cpfDB,
       phone: phoneDB,
       password: passwordDB,
-    } = await knex("users").where({ id }).first();
+    } = userDB;
 
-    if (!(emailDB === email)) {
-      const emailExist = await knex("users")
-        .select("email")
-        .where({ email })
-        .first();
-      
-      if (emailExist) {
-        return res
-          .status(404)
-          .json({ user: { email: "E-mail já cadastrado" } });
-      }
-      newUserData.email = email;
-    }
+    const emailExist = await knex("users")
+      .where({ email })
+      .whereNot({ id })
+      .first();
 
-    if (!(cpfDB === cpf) && cpf !== undefined) {
-      const cpfExist = await knex("users").select("cpf").where({ cpf }).first();
-      if (cpfExist) {
-        return res.status(404).json({ user: { cpf: "CPF já cadastrado" } });
-      }
-      newUserData.cpf = cpf;
-    }
-
-    if (!(phoneDB === phone) && phone !== undefined) {
-      newUserData.phone = phone;
-    }
-    if (!(nameDB === name)) {
-      newUserData.name = name;
-    }
+    const cpfExist = await knex("users")
+      .where({ cpf })
+      .whereNot({ id })
+      .first();
 
     if (password) {
       const checkPassword = await bcrypt.compare(password, passwordDB);
       if (checkPassword) {
-        return res.status(404).json({
-          user: { password: "A senha deve ser diferente da anterior" },
-        });
+        errors.password = "A senha deve ser diferente da anterior";
+      } else {
+        const newPasswordEncrypted = await bcrypt.hash(password, 10);
+        newUserData.password = newPasswordEncrypted;
       }
-
-      const newPasswordEncrypted = await bcrypt.hash(password, 10);
-      newUserData.password = newPasswordEncrypted;
     }
 
-    if (!Object.keys(newUserData)[0]) {
-      return res
-        .status(200)
-        .json({ user: { success: "Usuário sem alterações" } });
+    if (emailExist) {
+      errors.email = "E-mail já cadastrado";
+    }
+    if (cpfExist) {
+      errors.cpf = "CPF já cadastrado";
+    }
+    if (errors.cpf || errors.email || errors.password) {
+      return res.status(400).json({ user: { error: errors } });
     }
 
-    await knex("users").where({ id }).update(newUserData);
-    return res
-      .status(200)
-      .json({ user: { success: "Usuário editado com sucesso" } });
+    if (emailDB !== email) {
+      newUserData.email = email;
+    }
+
+    if (cpfDB !== cpf && cpf !== undefined) {
+      newUserData.cpf = cpf;
+    }
+
+    if (phoneDB !== phone && phone !== undefined) {
+      newUserData.phone = phone;
+    }
+    if (nameDB !== name) {
+      newUserData.name = name;
+    }
+    const { password: oldPassword, ...userData } = userDB;
+    const newUserDB = { ...userData, ...newUserData };
+
+    if (Object.keys(newUserData)[0]) {
+      await knex("users").where({ id }).update(newUserData);
+    }
+
+    return res.status(200).json({ user: newUserDB });
   } catch (error) {
-    return res.status(400).json(error.message);
+    return res.status(404).json(error.message);
   }
 };
 
