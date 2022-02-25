16db561fb6f60cbc9e3acd00966f7a083d9261f1
fiusks <rafabarros1@hotmail.com>
2022-02-25 18:01:42 -0300

    implementação do toast de sucesso e falha

--- src/components/toast/index.jsx
+++ src/components/toast/index.jsx
@@ -1,13 +1,19 @@
 import "./style.scss";
 import succesToastIcon from "../../assets/images/successtoasticon.svg";
+import failToastIcon from "../../assets/images/falha-icon.svg";
 import closeIcon from "../../assets/images/closeicon.svg";
 import useUser from "../../hooks/useUser";
 
 export default function ToastComponent() {
-  const { setShowToast, toastMessage } = useUser();
+  const { setShowToast, toastMessage, toastType } = useUser();
+  console.log(toastType);
+
   return (
-    <div className="toast-success-container">
-      <img src={succesToastIcon} alt="success icon" />
+    <div className={`generic-toast-container ${toastType} `}>
+      <img
+        src={toastType === "success" ? succesToastIcon : failToastIcon}
+        alt={`${toastType} icon`}
+      />
       <p>{toastMessage}</p>
       <img
         onClick={() => setShowToast(false)}
