0289b309ce76a66976f9aabda734d6647fe04716
fiusks <rafabarros1@hotmail.com>
2022-02-25 17:17:23 -0300

    refatoração da rota para o modelo padronizado

--- src/controllers/bills/editBill.js
+++ src/controllers/bills/editBill.js
@@ -4,18 +4,23 @@ const editBill = async (req, res) => {
   try {
     const { id, clientId, amount, status, dueDate, desc } = req.body.bill;
 
-    const hasBill = await knex("bills").where({
-      id: id,
-      client_id: clientId,
-    });
-    if (hasBill.length === 0) {
-      return res.status(404).json({ message: "Bill not found" });
+    const billExist = await knex("bills")
+      .where({
+        id: id,
+        client_id: clientId,
+      })
+      .first();
+
+    if (!billExist) {
+      return res
+        .status(404)
+        .json({ bill: { message: "Cobrança não encontrada" } });
     }
 
     const response = await knex("bills")
       .where({ id })
       .update({
-        amount: amount,
+        amount,
         bill_status: status,
         client_id: clientId,
         description: desc,
@@ -27,8 +32,10 @@ const editBill = async (req, res) => {
         .status(400)
         .json({ message: "it was not possible to register a edited billing" });
     }
-    
-    return res.status(200).json({ message: "sucess" });
+
+    return res
+      .status(200)
+      .json({ bill: { message: "Cobrança editada com sucesso" } });
   } catch (error) {
     return res.status(400).json(error.message);
   }
