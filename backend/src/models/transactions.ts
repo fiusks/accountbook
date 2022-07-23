feadfce14fe969285a498166579676d16155ca86
Rafael <rafabarros1@hotmail.com>
2022-07-22 21:00:27 -0300

    FIX: rename variables and add needed ones

--- backend/src/models/transactions.ts
+++ backend/src/models/transactions.ts
@@ -2,10 +2,10 @@ export interface ITransaction {
     id: number,
     amount: number,
     type: "credit" | "debit"
-    date: Date
-    due_date: Date | null
-    status: "paid" | "pending" | null
-    user_id: number
+    due_date: Date
+    status: "paid" | "pending" | "overdue" | null
+    description: string
+    client_id: number
     created_at?: Date
     updated_at?: Date
 }
